1f5467cd039994dcc0e22bb2be8508a5be6fab55
lttl-amethyst <heloisaflorsilvadearaujo@gmail.com>
2026-03-20 11:17:32 -0300

    atv

---
+++ 20.03/atv.js
@@ -0,0 +1,56 @@
+/*const hora = 14;
+const saudacao = hora < 12 ? "Bom dia" : hora < 18 ? "Boa tarde" : "Boa noite";
+console.log(saudacao);
+
+let resultado = 0;
+for (let i = 1; i <= 3; i++) {
+   resultado += i;
+}
+console.log(resultado);*/
+
+/*const dobrar = n => n * 2;
+console.log(dobrar(5) + dobrar(2));*/
+
+/*const frutas = ["Maçã", "Banana"];
+frutas.push("Laranja");
+console.log(frutas.length);*/
+
+/*const numeros = new Set([1, 2, 2, 3]);
+console.log(numeros.size);*/
+
+// const elemento = document.getElementById("caixa");
+// elemento.textContent = "Mundo";
+// console.log(elemento.textContent);
+
+/*class Pessoa {
+    constructor(nome) {
+        this.nome;
+    }
+}
+const usuario = new Pessoa("Ana");
+console.log(typeof usuario);*/
+
+/*const carro = { marca: "Toyota", ano: 2024 };
+console.log(carro["marca"]);*/
+
+/*const numeros = [10, 15, 20, 25, 30];
+let soma = 0;
+
+for(let i =0; i <numeros.length; i++) {
+    if (numeros[i] % 2 === 0 && numeros[i] > 15) {
+        soma += numeros[i];
+    }
+}
+console.log(soma);*/
+
+/*const usuarios = [
+    {nome: 'Rui', ativo: true},
+    {nome: 'Ana', ativo: false},
+    {nome: 'Bia', ativo: true}
+  ];
+
+    const resultado= usuarios
+    .filter(u => u.ativo)
+    .map(u => nome);
+
+  console.log(resultado);*/
