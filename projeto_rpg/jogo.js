004979b9503ccb7a27a3be05b2ddd9cd8095c750
lttl-amethyst <heloisaflorsilvadearaujo@gmail.com>
2026-03-23 12:14:05 -0300

    blublublu

--- projeto_rpg/jogo.js
+++ projeto_rpg/jogo.js
@@ -8,7 +8,31 @@ class Personagem {
     }
 
     atacar(alvo, habilidade) {
-
+        //verificar mana suficiente
+        if(this.mana >= habilidade.custo && this.energia >= habilidade.energia) {
+            alvo.hp -= habilidade.dano;
+            //debitar mana
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo;
+                this.energia += 50;
+            }
+            //esvaziar energia
+            if (habilidade.energia > 0) {
+                this.energia = 0;
+            }
+            //retornar msg
+            return `${this.nome} usou ${habilidade.nome}`;
+        } else {
+            return `sem mana ou energia para usar ${habilidade.nome}`
+        }
+    }
+    boss_atacar(alvo){
+        if(this.energia == 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
     }
 }
 
@@ -33,6 +57,17 @@ document.getElementById("titulo-heroi").textContent = `⚔️ ${hero.titulo}`;
 document.getElementById("nome-boss").textContent = `${boss.nome}`;
 document.getElementById("titulo-boss").textContent = `💀 ${boss.titulo}`;
 
+//atualizar as barras de vida
+const atualizarInterface = (mensagem) => {
+    //barras heroi
+    document.getElementById("hp-heroi").value = hero.hp;
+    document.getElementById("mp-heroi").value = hero.mana;
+    document.getElementById("en-heroi").value = hero.energia;
+    //barras boss
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("en-boss").value = boss.energia;
+}
+
 // Criar habilidades e botões
 let container = document.getElementById("controles");
 let listaHabilidades = [
@@ -43,6 +78,11 @@ let listaHabilidades = [
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");
     btn.innerText = hab.nome;
-    btn.classList.add("btn bnt-dark");
+    btn.classList.add("btn", "btn-dark");
     container.appendChild(btn);
-})
+    btn.onclick = () => {
+        let mensagem = hero.atacar(boss, hab);
+        atualizarInterface(mensagem);
+        boss.boss_atacar(hero);
+    }
+});
