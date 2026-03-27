2ae760e24e6bfa2dd68ef764d24cdde4db915d86
lttl-amethyst <heloisaflorsilvadearaujo@gmail.com>
2026-03-27 12:13:45 -0300

    bubu

---
+++ app_01/src/actions.ts
@@ -0,0 +1,7 @@
+import { Livro } from  './livro';
+import { biblioteca } from './database';
+
+//buscar livro
+export function buscarLivroPorId(id: number): Livro | undefined {
+    return biblioteca.find(livro => livro.id == id);
+}
