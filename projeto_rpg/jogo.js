a5517c797beca0883c51a415649bceaa0ae4e07b
lttl-amethyst <heloisaflorsilvadearaujo@gmail.com>
2026-03-27 08:48:09 -0300

    rpgbubu

--- projeto_rpg/jogo.js
+++ projeto_rpg/jogo.js
@@ -10,6 +10,7 @@ class Personagem {
     atacar(alvo, habilidade) {
         //verificar mana suficiente
         if(this.mana >= habilidade.custo && this.energia >= habilidade.energia) {
+            //dano no alvo
             alvo.hp -= habilidade.dano;
             //debitar mana
             if (habilidade.custo > 0) {
@@ -22,16 +23,19 @@ class Personagem {
             }
             //retornar msg
             return `${this.nome} usou ${habilidade.nome}`;
-        } else {
+            } else {
             return `sem mana ou energia para usar ${habilidade.nome}`
         }
     }
-    boss_atacar(alvo){
+
+    boss_atacar(alvo) {
         if(this.energia == 100) {
             alvo.hp -= 15;
             this.energia = 0;
+            return ` Lich usou sua habilidade `
         } else {
             this.energia += 50;
+            return ` Lich carregou o ataque `
         }
     }
 }
@@ -57,15 +61,32 @@ document.getElementById("titulo-heroi").textContent = `⚔️ ${hero.titulo}`;
 document.getElementById("nome-boss").textContent = `${boss.nome}`;
 document.getElementById("titulo-boss").textContent = `💀 ${boss.titulo}`;
 
-//atualizar as barras de vida
-const atualizarInterface = (mensagem) => {
+// Criar array de turnos
+const turnos = ["Aguardando Ação"];
+
+//Atualizar as barras de vida
+const atualizarInterface = (msg_hero, msg_boss)=> {
+    
     //barras heroi
     document.getElementById("hp-heroi").value = hero.hp;
     document.getElementById("mp-heroi").value = hero.mana;
     document.getElementById("en-heroi").value = hero.energia;
+
     //barras boss
+    console.log(boss.energia)
     document.getElementById("hp-boss").value = boss.hp;
     document.getElementById("en-boss").value = boss.energia;
+
+    //turnos
+    document.getElementById("log-hero").textContent = msg_hero;
+    document.getElementById("log-boss").textContent = msg_boss;
+
+    if (boss.hp<= 0){
+        document.getElementById("tela").innerHTML = "Você venceu!";
+    }
+    if (hero.hp<= 0){
+        document.getElementById("tela").innerHTML = "Você perdeu!";
+    }
 }
 
 // Criar habilidades e botões
@@ -75,14 +96,16 @@ let listaHabilidades = [
     new Habilidade(2, "skill", 8, 10, 0),
     new Habilidade(3, "ultimate", 15, 0, 100)
 ];
+
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");
     btn.innerText = hab.nome;
     btn.classList.add("btn", "btn-dark");
     container.appendChild(btn);
     btn.onclick = () => {
-        let mensagem = hero.atacar(boss, hab);
-        atualizarInterface(mensagem);
-        boss.boss_atacar(hero);
+        let msg_hero = hero.atacar(boss, hab);
+        let msg_boss = boss.boss_atacar(hero);
+        atualizarInterface(msg_hero, msg_boss);
+        
     }
 });
